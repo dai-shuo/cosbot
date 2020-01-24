8581223c74eebadad5dd33885965efdf9d2a9bfe
daishuo <daishuo@gmail.com>
2020-01-24 16:24:23 +0800

    default listening port->8000

--- cosbot.go
+++ cosbot.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	notifier := NewMessageNotifier(":9876")
+	notifier := NewMessageNotifier(":8000")
 	notifier.AddCallback(func(chatId, userId, msgId, text string) {
 		fmt.Printf("chatId=%s, userId=%s, msgId=%s, text=%s\n", chatId, userId, msgId, text)
 	})
