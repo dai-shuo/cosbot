a5389748a4e4e2b4aa0144a67a590bcf473bd999
daishuo <daishuo@gmail.com>
2020-01-24 16:11:47 +0800

    simple recv msg demo

--- cosbot.go
+++ cosbot.go
@@ -1,6 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	router := buildRouter()
-	_ = router.Run(":9876")
+	notifier := NewMessageNotifier(":9876")
+	notifier.AddCallback(func(chatId, userId, msgId, text string) {
+		fmt.Printf("chatId=%s, userId=%s, msgId=%s, text=%s\n", chatId, userId, msgId, text)
+	})
+	_ = notifier.Run()
 }
